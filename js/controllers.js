8a13cae4aa32f755244345fa614faf7e25649aaf
Michael Rapadas <mike@mrap.me>
2014-03-03 21:53:45 -0800

    Adds portfolio.

--- js/controllers.js
+++ js/controllers.js
@@ -21,10 +21,33 @@ controllers.controller('SkillsCtrl', function($scope){
 });
 
 controllers.controller('PortfolioCtrl', function($scope){
-  $scope.workSamples = [
+  $scope.workSamples = [];
+  $scope.workSamples.push(
     {
       name: "Spot",
-      link: "https://itunes.apple.com/us/app/spot-places-right-now/id820068868?ls=1&mt=8"
+      link: "https://itunes.apple.com/us/app/spot-places-right-now/id820068868?ls=1&mt=8",
+      photoUrl: "http://25.media.tumblr.com/11efc3686d380f349dbf5a75583ba30a/tumblr_mzmpn2UExS1sp583mo1_1280.jpg"
     }
-  ];
+  );
+  $scope.workSamples.push(
+    {
+      name: "Battery Hero",
+      link: "https://itunes.apple.com/us/app/battery-hero/id782015477?mt=12",
+      photoUrl: "https://mrap-blog.s3.amazonaws.com/battery-hero-screenshot-tint.jpg"
+    }
+  );
+  $scope.workSamples.push(
+    {
+      name: "Sunriser",
+      link: "https://itunes.apple.com/de/app/sunriser/id684789140?l=en&mt=8",
+      photoUrl: "https://mrap-blog.s3.amazonaws.com/sunriser-screenshot.jpg"
+    }
+  );
+  $scope.workSamples.push(
+    {
+      name: "Coffeeshop",
+      link: "http://doingnow.herokuapp.com/",
+      photoUrl: "https://mrap-blog.s3.amazonaws.com/coffeeshop-tint.jpg"
+    }
+  );
 });
